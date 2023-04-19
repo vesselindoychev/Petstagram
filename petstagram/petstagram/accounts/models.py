144d483b43c2929d0d4d90c6eaaf9b38f0132660
Veselin Doychev <veselindoi123@gmail.com>
2023-04-19 13:33:17 +0300

    Created User and Profile Models

--- petstagram/petstagram/accounts/models.py
+++ petstagram/petstagram/accounts/models.py
@@ -1,3 +1,80 @@
+from django.contrib.auth import models as auth_models
+from django.core.validators import MinLengthValidator
 from django.db import models
 
-# Create your models here.
+from petstagram.accounts.managers import PetstagramUserManager
+from petstagram.common.custom_validators import validate_only_letters
+
+
+class PetstagramUser(auth_models.AbstractBaseUser, auth_models.PermissionsMixin):
+    email = models.EmailField(
+        unique=True,
+    )
+
+    date_joined = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    is_staff = models.BooleanField(
+        default=False,
+    )
+
+    USERNAME_FIELD = 'email'
+
+    objects = PetstagramUserManager()
+
+
+class Profile(models.Model):
+    FIRST_NAME_MIN_LENGTH = 2
+    FIRST_NAME_MAX_LENGTH = 30
+    LAST_NAME_MIN_LENGTH = 2
+    LAST_NAME_MAX_LENGTH = 30
+
+    MALE = 'Male'
+    FEMALE = 'Female'
+    DO_NOT_SHOW = 'Do not show'
+
+    GENDERS = (
+        MALE, FEMALE, DO_NOT_SHOW
+    )
+
+    first_name = models.CharField(
+        max_length=FIRST_NAME_MAX_LENGTH,
+        validators=(
+            MinLengthValidator(FIRST_NAME_MIN_LENGTH),
+            validate_only_letters,
+        )
+    )
+
+    last_name = models.CharField(
+        max_length=LAST_NAME_MAX_LENGTH,
+        validators=(
+            MinLengthValidator(LAST_NAME_MIN_LENGTH),
+            validate_only_letters,
+        )
+    )
+
+    picture = models.URLField()
+
+    date_of_birth = models.DateField(
+        null=True,
+        blank=True,
+    )
+
+    description = models.TextField(
+        null=True,
+        blank=True,
+    )
+
+    gender = models.CharField(
+        max_length=(max(len(g) for g in GENDERS)),
+        choices=((g, g) for g in GENDERS),
+        null=True,
+        blank=True
+    )
+
+    user = models.OneToOneField(
+        PetstagramUser,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
