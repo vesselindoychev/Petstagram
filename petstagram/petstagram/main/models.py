86aeace982a6adfc43dcbad9a51e498744114a19
Veselin Doychev <veselindoi123@gmail.com>
2023-04-19 14:14:25 +0300

    Added Pet Model

--- petstagram/petstagram/main/models.py
+++ petstagram/petstagram/main/models.py
@@ -1,3 +1,44 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+UserModel = get_user_model()
+
+
+class Pet(models.Model):
+    NAME_MAX_LENGTH = 30
+    CAT = 'Cat'
+    DOG = 'Dog'
+    BUNNY = 'Bunny'
+    PARROT = 'Parrot'
+    FISH = 'Fish'
+    OTHER = 'Other'
+    ANIMALS = (
+        CAT,
+        DOG,
+        BUNNY,
+        PARROT,
+        FISH,
+        OTHER,
+    )
+
+    name = models.CharField(
+        max_length=NAME_MAX_LENGTH,
+    )
+
+    type = models.CharField(
+        max_length=(max(len(a) for a in ANIMALS)),
+        choices=((a, a) for a in ANIMALS),
+    )
+
+    date_of_birth = models.DateField(
+        null=True,
+        blank=True,
+    )
+
+    user = models.ForeignKey(
+        UserModel,
+        on_delete=models.CASCADE,
+    )
+
+    class Meta:
+        unique_together = ('user', 'name')
